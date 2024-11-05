71c782311a5ad3296a3d01b0e2ad73366cb0e1ab
jameswuu <jameswlb01@gmail.com>
2024-11-05 10:18:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 # This comment has been added 
 print("Hello World!")
-print("Today is a good day!")
+print("Today is a great day!")
+
+
+# New code to print numbers
+numbers = [1,2,3,4,5]
+for number in numbers:
+  print(number)
