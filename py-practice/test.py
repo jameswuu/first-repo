cae6d0376b8408aca99cf1b9dda01a9459506920
James Wu <jameswlb01@gmail.com>
2024-11-06 09:49:18 +0800

    file added

---
+++ py-practice/test.py
@@ -0,0 +1,15 @@
+"""
+The purpose of this file is to familize myself with the git environmnet
+Features such as pull, push, commit were practiced using this file
+"""
+
+# This comment has been added 
+print("Hello World!")
+print("Today is a great day!")
+
+
+# New code to print numbers
+numbers = [1,2,3,4,5,6,7,8]
+for number in numbers:
+  if number <= 4:
+      print(number)
