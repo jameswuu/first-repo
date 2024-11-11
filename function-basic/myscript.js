5c95168596ed28809406cb169146f31e4672e4a5
James Wu <jameswlb01@gmail.com>
2024-11-11 21:15:36 +0800

    Add capitalize function to JS

--- function-basic/myscript.js
+++ function-basic/myscript.js
@@ -5,8 +5,19 @@ function add7(number) {
 }
 
 
-// Write function taeks 2 numbers and reutres their product
+// Write a function takes 2 numbers and reutres their product
 function multiply(num1, num2) {
     let answer = (parseInt(num1) * parseInt(num2));
     return answer;
 } 
+
+
+// Write a function takes a string and returns with only the first letter capitalized
+function capitalize(word) {
+    // Handle empty string
+    if (!word || typeof word !== 'string'){
+        return 'Invalid Input';
+    } else {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+}
