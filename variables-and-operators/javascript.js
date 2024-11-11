375273024a6594721df27d8234b6564cf82782d0
James Wu <jameswlb01@gmail.com>
2024-11-11 11:04:20 +0800

    Create JS for index file

---
+++ variables-and-operators/javascript.js
@@ -0,0 +1,27 @@
+// Ways to print thins to the devloper console 
+console.log("Hello World!") 
+
+// Declare variables and print them
+let name = "James";
+let surename = "Bond";
+console.log(name);
+console.log(surename);
+
+// exercise 1: Simple addition
+console.log(23+77)
+
+// exercise 2: Simple Calculation
+console.log((4+6+9)/77)
+
+// exercise 3: functions and numbers
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+// exercise 4: intermediate calculation
+let max = 57;
+let actual = max - 13;
+let percentage = actual/max;
+console.log(percentage);
