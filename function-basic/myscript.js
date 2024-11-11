ed9953ff6d75d53838d347a6466e2d260c5d3066
James Wu <jameswlb01@gmail.com>
2024-11-11 20:57:43 +0800

    Add multiply function to calculate the product of two numbers

--- function-basic/myscript.js
+++ function-basic/myscript.js
@@ -1,5 +1,12 @@
-// Write a function takes a number and returns number (+7)
+// Write a function takes a number and returns number + 7
 function add7(number) {
     let answer = (parseInt(number) + 7 );
-    return answer
+    return answer;
 }
+
+
+// Write function taeks 2 numbers and reutres their product
+function multiply(num1, num2) {
+    let answer = (parseInt(num1) * parseInt(num2));
+    return answer;
+} 
