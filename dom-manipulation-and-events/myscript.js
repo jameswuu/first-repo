0d8edaf15a32e0b42592122181125149f8139577
James Wu <jameswlb01@gmail.com>
2024-11-14 10:59:41 +0800

    Update the JS and index

--- dom-manipulation-and-events/myscript.js
+++ dom-manipulation-and-events/myscript.js
@@ -1,8 +1,23 @@
 // My JS file
+
+// Select container element
 const container = document.querySelector("#container");
 
+
+// Example 0:
+// Add element to container 
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-
+// Append content to container 
 container.appendChild(content);
+
+
+// Example 1:
+// Add <p> element
+const paragraph = document.createElement("p");
+paragraph.style.color = "red";
+paragraph.textContent = "Hey I am red";
+// Append content to container 
+container.appendChild(paragraph);
+
