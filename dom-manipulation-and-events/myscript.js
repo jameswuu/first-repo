2b9fd96d631d110be329059fd895f5ec5cfc103c
James Wu <jameswlb01@gmail.com>
2024-11-14 11:05:36 +0800

    Add example 2: h3 element

--- dom-manipulation-and-events/myscript.js
+++ dom-manipulation-and-events/myscript.js
@@ -21,3 +21,11 @@ paragraph.textContent = "Hey I am red";
 // Append content to container 
 container.appendChild(paragraph);
 
+
+// Example 2:
+// Add <h3> element
+const header_3 = document.createElement("h3");
+header_3.style.color = "blue";
+header_3.textContent = "I am a blue h3";
+// Append to the container
+container.insertBefore(header_3, paragraph);
