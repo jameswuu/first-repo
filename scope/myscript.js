895137591312f4818f36f703adf64fda434d7c32
James Wu <jameswlb01@gmail.com>
2024-12-03 13:22:13 +0800

    Create new file

---
+++ scope/myscript.js
@@ -0,0 +1,10 @@
+// let cool;
+// if (1 === 1) {
+//     cool = true;
+//   }
+//   console.log(cool);
+
+function logDog(dog){
+    dog = "whateverYouPassedInAsTheFirstArgumentToTheFunction";
+    console.log(dog)
+  }
