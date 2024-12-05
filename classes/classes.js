d1ffc864f0fd2c7b2cbcfd0fca28852e3c6542b7
James Wu <jameswlb01@gmail.com>
2024-12-05 14:42:22 +0800

    Add extends and static method

--- classes/classes.js
+++ classes/classes.js
@@ -1,5 +1,15 @@
 // Classes
 class Person{
+    // static method (Only store in the class)
+    // Won't be inherited to the instance
+    static species () {
+        return "Homo Sapiens"
+    } 
+
+    static speciesSentense() {
+        return `Humen are classified as ${this.species()}`
+    }
+
     constructor(firstName, lastName){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -20,5 +30,19 @@ class Person{
 
     jobStatus(){
         return this.hasJob = !this.hasJob;
+    }
+}
+
+
+// Extending from the Person's class
+class Worker extends Person{
+    constructor(firstName, lastName, job){
+        super(firstName, lastName); // Referrring to the construtor from the extended Person (All initialization is set in the Perspm's constructor)
+        this.job = job;
+        this.hasJob = true;
+    }
+
+    setJob (job) {
+        this.job = job;
     }
 }
