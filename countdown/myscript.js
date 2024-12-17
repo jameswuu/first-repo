9e52eb08666e62459d3d94c81148bbbecf206624
James Wu <jameswlb01@gmail.com>
2024-12-17 20:48:48 +0800

    Create countdown

---
+++ countdown/myscript.js
@@ -0,0 +1,4 @@
+// Tasks
+// Using JS and HTML, write a countdown timer with the ability to set 
+// custom countdown duration, pause / resume the timer, and reset the timer
+// Bonus: track and display previous results and the timestamp the record was made
