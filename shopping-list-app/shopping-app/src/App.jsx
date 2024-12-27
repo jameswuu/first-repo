ad3e4b752612b8c049dfb5df40210f595f3ec134
James Wu <jameswlb01@gmail.com>
2024-12-27 16:51:25 +0800

    Add UseEffect to keep track of the change

--- shopping-list-app/shopping-app/src/App.jsx
+++ shopping-list-app/shopping-app/src/App.jsx
@@ -2,7 +2,8 @@ import { Header } from "./components/Header"
 import { Tabs } from "./components/Tabs"
 import { ShoppingList } from "./components/ShoppingList"
 import { ShoppingInput } from "./components/ShoppingInput"
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
 
 function App() {
   const [tobuy, setTobuy] = useState(
@@ -17,28 +18,52 @@ function App() {
 
   const [selectedTab, setSelectedTab] = useState("All")
 
+
+  // Functions
   function handleAddTobuy(input){
-    let newTobuyList = [...tobuy, {item: input, purchased: false}]
-    setTobuy(newTobuyList)
+    let newTobuyList = [...tobuy, {item: input, purchased: false}];
+    setTobuy(newTobuyList);
+    handleSaveList(newTobuyList);
   }
   
   function handleEditTobuy(index){
     // Get the list's index
-    let newTobuyList = [...tobuy]
-    let editTobuy = newTobuyList[index]
+    let newTobuyList = [...tobuy];
+    let editTobuy = newTobuyList[index];
 
     // Update and replace the list
-    editTobuy["purchased"] = true
-    newTobuyList[index] = editTobuy
-    setTobuy(newTobuyList)
+    editTobuy["purchased"] = true;
+    newTobuyList[index] = editTobuy;
+    setTobuy(newTobuyList);
+    handleSaveList(newTobuyList);
   }
   
   function handleDeleteTobuy(index){
     let newTobuyList = tobuy.filter((val, valIndex)=> {
       return(valIndex !== index)
     })
-    setTobuy(newTobuyList)
+    setTobuy(newTobuyList);
+    handleSaveList(newTobuyList);
   }
+
+
+  // Save the data
+  function handleSaveList(currentTobuy){
+    localStorage.setItem('tobuy-app', JSON.stringify ({
+      tobuys: currentTobuy
+    }))
+  }
+
+  // Use Effect: Using to track events 
+  useEffect(()=>{
+    if(!localStorage || !localStorage.getItem('tobuy-app')) {
+      return
+    }
+    let db = JSON.parse(localStorage.getItem('tobuy-app'))
+    console.log(db)
+    setTobuy(db.tobuys)
+  }, [])
+
 
   return(
     <>
